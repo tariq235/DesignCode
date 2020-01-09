5c34de54a7b575f71257f48c274f225a93e14401
Tariq <tmohamma@visa.com>
2020-01-09 14:26:58 +0800

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Tariq Mohammad on 1/9/20.
+//  Copyright © 2020 Tariq Mohammad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
